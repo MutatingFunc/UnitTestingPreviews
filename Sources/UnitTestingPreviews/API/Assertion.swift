677aeeff4d56ae26208965df6a750b8b240f5cf2
James Froggatt <james.froggatt@me.com>
2022-12-03 23:56:49 +0000

    Conform Never to Assertion

--- Sources/UnitTestingPreviews/API/Assertion.swift
+++ Sources/UnitTestingPreviews/API/Assertion.swift
@@ -3,3 +3,15 @@ import SwiftUI
 public protocol Assertion: View, CustomStringConvertible {
     var condition: Bool {get}
 }
+
+extension Never: Assertion {
+    public var condition: Bool {
+        switch self {}
+    }
+    public var description: String {
+        switch self {}
+    }
+    var body: EmptyView {
+        switch self {}
+    }
+}
